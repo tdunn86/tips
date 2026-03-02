26a0bf8ec5ac51b3cb2a22d4303b8bf253dfb766
oben11 <dithskii@gmail.com>
2026-03-01 21:41:17 -0500

    DataWriter branch and stub

---
+++ DataWriter.java
@@ -0,0 +1,18 @@
+package tips;
+
+/**
+ * This class is responsible for saving user data and questions.
+ * It contains methods to save user information and questions to a database or file.
+ */
+
+public class DataWriter {
+
+    private void saveUser() {
+        // Code to save user data
+    }
+
+    private void saveQuestions() {
+        // Code to save user data
+    }
+    
+}
