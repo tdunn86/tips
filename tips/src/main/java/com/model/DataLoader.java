8bb625298178165c05e0d8eac139e167edcf7198
Samuelbsk8 <Sammybsk8@gmail.com>
2026-03-03 22:30:06 -0500

    dataloader changes

--- tips/src/main/java/com/model/DataLoader.java
+++ tips/src/main/java/com/model/DataLoader.java
@@ -22,17 +22,22 @@ public class DataLoader extends DataConstants {
 
                 JSONObject userObject = (JSONObject) obj;
 
-                int userId = ((Long) userObject.get("userId")).intValue();
-                String username = (String) userObject.get("username");
-                String password = (String) userObject.get("password");
-                String email = (String) userObject.get("email");
-                String userType = (String) userObject.get("userType");
+                int userId      = ((Long) userObject.get(USER_ID)).intValue();
+                String username = (String) userObject.get(USER_USERNAME);
+                String password = (String) userObject.get(USER_PASSWORD);
+                String email    = (String) userObject.get(USER_EMAIL);
+                String userType = (String) userObject.get(USER_TYPE);
 
                 User user = null;
 
                 switch (userType) {
                     case "Student":
-                        user = new Student(userId, username, password, email);
+                        Student student = new Student(userId, username, password, email);
+                        if (userObject.containsKey(USER_CLASSIFICATION))
+                            student.setClassification((String) userObject.get(USER_CLASSIFICATION));
+                        if (userObject.containsKey(USER_STREAK))
+                            student.setStreak(((Long) userObject.get(USER_STREAK)).intValue());
+                        user = student;
                         break;
 
                     case "Editor":
@@ -44,7 +49,8 @@ public class DataLoader extends DataConstants {
                         break;
                 }
 
-                users.add(user);
+                if (user != null)
+                    users.add(user);
             }
 
         } catch (Exception e) {
@@ -56,6 +62,7 @@ public class DataLoader extends DataConstants {
     public static ArrayList<Question> getQuestions() {
 
         ArrayList<Question> questions = new ArrayList<>();
+        ArrayList<User> users = getUsers();
 
         try {
             FileReader reader = new FileReader(QUESTION_FILE_NAME);
@@ -66,23 +73,74 @@ public class DataLoader extends DataConstants {
 
                 JSONObject questionObject = (JSONObject) obj;
 
-                String title = (String) questionObject.get("title");
-                String prompt = (String) questionObject.get("prompt");
-                String difficultyStr = (String) questionObject.get("difficulty");
-                String languageStr = (String) questionObject.get("language");
-                String courseStr = (String) questionObject.get("course");
+                String title         = (String) questionObject.get(QUESTION_TITLE);
+                String prompt        = (String) questionObject.get(QUESTION_PROMPT);
+                String difficultyStr = (String) questionObject.get(QUESTION_DIFFICULTY);
+                String languageStr   = (String) questionObject.get(QUESTION_LANGUAGE);
+                String courseStr     = (String) questionObject.get(QUESTION_COURSE);
 
                 Difficulty difficulty = Difficulty.valueOf(difficultyStr);
-                Language language = Language.valueOf(languageStr);
-                Course course = Course.valueOf(courseStr);
+                Language language     = Language.valueOf(languageStr);
+                Course course         = Course.valueOf(courseStr);
 
-                Question question = new Question(
-                        title,
-                        prompt,
-                        difficulty,
-                        language,
-                        course
-                );
+                Question question = new Question(title, prompt, difficulty, language, course);
+
+                // Optional fields
+                if (questionObject.containsKey(QUESTION_SAMPLE_SOL))
+                    question.setSampleSolution((String) questionObject.get(QUESTION_SAMPLE_SOL));
+
+                if (questionObject.containsKey(QUESTION_SAMPLE_EXP))
+                    question.setSampleExplanation((String) questionObject.get(QUESTION_SAMPLE_EXP));
+
+                if (Boolean.TRUE.equals(questionObject.get(QUESTION_REVEALED)))
+                    question.revealSolution();
+
+                if (questionObject.containsKey(QUESTION_AUTHOR_ID)) {
+                    int authorId = ((Long) questionObject.get(QUESTION_AUTHOR_ID)).intValue();
+                    findUserById(users, authorId).ifPresent(question::setAuthor);
+                }
+
+                // Solutions
+                if (questionObject.containsKey("solutions")) {
+                    JSONArray solutionsJSON = (JSONArray) questionObject.get("solutions");
+                    for (Object sObj : solutionsJSON) {
+                        JSONObject solutionObject = (JSONObject) sObj;
+
+                        int authorId   = ((Long) solutionObject.get(SOLUTION_AUTHOR_ID)).intValue();
+                        String content = (String) solutionObject.get(SOLUTION_CONTENT);
+
+                        findUserById(users, authorId).ifPresent(author -> {
+                            Solution solution = new Solution(author, question, content);
+                            if (solutionObject.containsKey(SOLUTION_UPVOTES))
+                                solution.setUpvotes(((Long) solutionObject.get(SOLUTION_UPVOTES)).intValue());
+                            if (Boolean.TRUE.equals(solutionObject.get(SOLUTION_ACCEPTED)))
+                                solution.setAccepted(true);
+                            question.addSolution(solution);
+                        });
+                    }
+                }
+
+                // Replies
+                if (questionObject.containsKey("replies")) {
+                    JSONArray repliesJSON = (JSONArray) questionObject.get("replies");
+                    for (Object rObj : repliesJSON) {
+                        JSONObject replyObject = (JSONObject) rObj;
+
+                        int authorId   = ((Long) replyObject.get(REPLY_AUTHOR_ID)).intValue();
+                        String content = (String) replyObject.get(REPLY_CONTENT);
+
+                        findUserById(users, authorId).ifPresent(author -> {
+                            Reply reply = new Reply(author, question, content);
+                            if (replyObject.containsKey(REPLY_TITLE))
+                                reply.setTitle((String) replyObject.get(REPLY_TITLE));
+                            if (replyObject.containsKey(REPLY_UPVOTES))
+                                reply.setUpvotes(((Long) replyObject.get(REPLY_UPVOTES)).intValue());
+                            if (Boolean.TRUE.equals(replyObject.get(REPLY_ACCEPTED)))
+                                reply.setAccepted(true);
+                            question.addReply(reply);
+                        });
+                    }
+                }
 
                 questions.add(question);
             }
@@ -92,4 +150,7 @@ public class DataLoader extends DataConstants {
 
         return questions;
     }
-}
+
+    private static java.util.Optional<User> findUserById(ArrayList<User> users, int userId) {
+        return users.stream().filter(u -> u.getUserId() == userId).findFirst();
+    }
