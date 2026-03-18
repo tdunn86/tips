08987f2ae8bae0422bfd9d0768e84b8b78cbf6ff
JGESSLER1 <jimmy.gessler3@gmail.com>
2026-03-18 11:31:06 -0400

    Fixed DataLoader/DataWriter for questions with recursive comments, added UUID fields to Reply and Solution, fixed logout to save users to JSON, fixed userType key mismatch, and added question browsing to TIPSUI

--- tips/src/main/java/com/model/DataLoader.java
+++ tips/src/main/java/com/model/DataLoader.java
@@ -2,31 +2,29 @@ package com.model;
 
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.UUID;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class DataLoader extends DataConstants {
- 
+
     public static ArrayList<User> getUsers() {
         ArrayList<User> users = new ArrayList<>();
- 
         try {
             FileReader reader = new FileReader(USER_FILE_NAME);
             JSONParser parser = new JSONParser();
             JSONArray userJSON = (JSONArray) parser.parse(reader);
- 
+
             for (Object obj : userJSON) {
                 JSONObject userObject = (JSONObject) obj;
- 
-                int    userId   = ((Long)   userObject.get(USER_ID)).intValue();
-                String username = (String)  userObject.get(USER_USERNAME);
-                String password = (String)  userObject.get(USER_PASSWORD);
-                String email    = (String)  userObject.get(USER_EMAIL);
-                String userType = (String)  userObject.get(USER_TYPE);
- 
+
+                int userId      = ((Long) userObject.get(USER_ID)).intValue();
+                String username = (String) userObject.get(USER_USERNAME);
+                String password = (String) userObject.get(USER_PASSWORD);
+                String email    = (String) userObject.get(USER_EMAIL);
+                String userType = (String) userObject.get(USER_TYPE);
+
                 User user = null;
                 switch (userType) {
                     case "Student":
@@ -44,82 +42,65 @@ public class DataLoader extends DataConstants {
                         user = new Admin(userId, username, password, email);
                         break;
                 }
- 
                 if (user != null) users.add(user);
             }
- 
         } catch (Exception e) {
-            System.out.println("EXCEPTION loading users: " + e.getMessage());
+            System.out.println("Error loading users: " + e.getMessage());
             e.printStackTrace();
         }
- 
         return users;
     }
- 
+
     public static ArrayList<Question> getQuestions() {
         ArrayList<Question> questions = new ArrayList<>();
         ArrayList<User> users = getUsers();
- 
+
         try {
             FileReader reader = new FileReader(QUESTION_FILE_NAME);
             JSONParser parser = new JSONParser();
             JSONArray questionJSON = (JSONArray) parser.parse(reader);
- 
+
             for (Object obj : questionJSON) {
                 JSONObject questionObject = (JSONObject) obj;
- 
+
                 String title         = (String) questionObject.get(QUESTION_TITLE);
                 String prompt        = (String) questionObject.get(QUESTION_PROMPT);
                 String difficultyStr = (String) questionObject.get(QUESTION_DIFFICULTY);
                 String languageStr   = (String) questionObject.get(QUESTION_LANGUAGE);
- 
-                String courseStr = (String) questionObject.get(QUESTION_COURSE);
- 
+                String courseStr     = (String) questionObject.get(QUESTION_COURSE);
+
                 Difficulty difficulty = Difficulty.valueOf(difficultyStr);
-                Language   language   = Language.valueOf(languageStr);
-                Course     course     = Course.valueOf(courseStr);
- 
+                Language language     = Language.valueOf(languageStr);
+                Course course         = Course.valueOf(courseStr);
+
                 Question question = new Question(title, prompt, difficulty, language, course);
- 
-                if (questionObject.containsKey(QUESTION_ID)) {
-                    question.setQuestionID(UUID.fromString(
-                        (String) questionObject.get(QUESTION_ID)));
-                }
- 
-                if (questionObject.containsKey(QUESTION_COURSES)) {
-                    JSONArray coursesArray = (JSONArray) questionObject.get(QUESTION_COURSES);
-                    for (Object c : coursesArray) {
-                        question.getCourses().add(Course.valueOf((String) c));
-                    }
-                }
- 
+
+                // Basic properties
                 if (questionObject.containsKey("hint"))
                     question.setHint((String) questionObject.get("hint"));
- 
+
                 if (questionObject.containsKey(QUESTION_SAMPLE_SOLUTION))
                     question.setSampleSolution((String) questionObject.get(QUESTION_SAMPLE_SOLUTION));
- 
+
                 if (questionObject.containsKey(QUESTION_SAMPLE_EXPLANATION))
                     question.setSampleExplanation((String) questionObject.get(QUESTION_SAMPLE_EXPLANATION));
- 
+
                 if (Boolean.TRUE.equals(questionObject.get(QUESTION_IS_SOLUTION_REVEALED)))
                     question.revealSolution();
- 
+
+                // Author
                 if (questionObject.containsKey(QUESTION_AUTHOR_ID)) {
                     int authorId = ((Long) questionObject.get(QUESTION_AUTHOR_ID)).intValue();
                     findUserById(users, authorId).ifPresent(question::setAuthor);
                 }
- 
-                // Solutions
+
+                // Solutions (answers)
                 if (questionObject.containsKey(QUESTION_SOLUTIONS)) {
                     JSONArray solutionsJSON = (JSONArray) questionObject.get(QUESTION_SOLUTIONS);
                     for (Object sObj : solutionsJSON) {
                         JSONObject solutionObject = (JSONObject) sObj;
- 
-                        if (!solutionObject.containsKey(SOLUTION_AUTHOR_ID)) continue;
-                        int    authorId = ((Long) solutionObject.get(SOLUTION_AUTHOR_ID)).intValue();
-                        String content  = (String) solutionObject.get(SOLUTION_CONTENT);
- 
+                        int authorId   = ((Long) solutionObject.get(SOLUTION_AUTHOR_ID)).intValue();
+                        String content = (String) solutionObject.get(SOLUTION_CONTENT);
                         findUserById(users, authorId).ifPresent(author -> {
                             Solution solution = new Solution(author, question, content);
                             if (solutionObject.containsKey(SOLUTION_UPVOTES))
@@ -130,43 +111,57 @@ public class DataLoader extends DataConstants {
                         });
                     }
                 }
- 
-                // Replies
+
+                // Replies (comments, recursive)
                 if (questionObject.containsKey(QUESTION_REPLIES)) {
                     JSONArray repliesJSON = (JSONArray) questionObject.get(QUESTION_REPLIES);
                     for (Object rObj : repliesJSON) {
-                        JSONObject replyObject = (JSONObject) rObj;
- 
-                        if (!replyObject.containsKey(REPLY_AUTHOR_ID)) continue;
-                        int    authorId = ((Long) replyObject.get(REPLY_AUTHOR_ID)).intValue();
-                        String content  = (String) replyObject.get(REPLY_CONTENT);
- 
-                        findUserById(users, authorId).ifPresent(author -> {
-                            Reply reply = new Reply(author, question, content);
-                            if (replyObject.containsKey(REPLY_TITLE))
-                                reply.setTitle((String) replyObject.get(REPLY_TITLE));
-                            if (replyObject.containsKey(REPLY_UPVOTES))
-                                reply.setUpvotes(((Long) replyObject.get(REPLY_UPVOTES)).intValue());
-                            if (Boolean.TRUE.equals(replyObject.get(REPLY_ACCEPTED)))
-                                reply.setAccepted(true);
-                            question.addReply(reply);
-                        });
+                        Reply reply = parseReply((JSONObject) rObj, question, users);
+                        if (reply != null) question.addReply(reply);
                     }
                 }
- 
+
                 questions.add(question);
             }
- 
         } catch (Exception e) {
-            System.out.println("EXCEPTION loading questions: " + e.getMessage());
+            System.out.println("Error loading questions: " + e.getMessage());
             e.printStackTrace();
         }
- 
         return questions;
     }
- 
+
+    /**
+     * Recursively parses a reply and any nested replies (comments on comments).
+     */
+    private static Reply parseReply(JSONObject replyObject, Question question, ArrayList<User> users) {
+        int authorId   = ((Long) replyObject.get(REPLY_AUTHOR_ID)).intValue();
+        String content = (String) replyObject.get(REPLY_CONTENT);
+
+        java.util.Optional<User> authorOpt = findUserById(users, authorId);
+        if (!authorOpt.isPresent()) return null;
+
+        Reply reply = new Reply(authorOpt.get(), question, content);
+
+        if (replyObject.containsKey(REPLY_TITLE))
+            reply.setTitle((String) replyObject.get(REPLY_TITLE));
+        if (replyObject.containsKey(REPLY_UPVOTES))
+            reply.setUpvotes(((Long) replyObject.get(REPLY_UPVOTES)).intValue());
+        if (Boolean.TRUE.equals(replyObject.get(REPLY_ACCEPTED)))
+            reply.setAccepted(true);
+
+        // Recursive: load nested replies (comments on comments)
+        if (replyObject.containsKey(QUESTION_REPLIES)) {
+            JSONArray nestedReplies = (JSONArray) replyObject.get(QUESTION_REPLIES);
+            for (Object nObj : nestedReplies) {
+                Reply nested = parseReply((JSONObject) nObj, question, users);
+                if (nested != null) reply.addReply(nested);
+            }
+        }
+
+        return reply;
+    }
+
     private static java.util.Optional<User> findUserById(ArrayList<User> users, int userId) {
         return users.stream().filter(u -> u.getUserId() == userId).findFirst();
     }
 }
- 
--- tips/src/main/java/com/model/DataWriter.java
+++ tips/src/main/java/com/model/DataWriter.java
@@ -1,31 +1,25 @@
 package com.model;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-
 /**
- * @author: Oliver Benjamin
+ * @author Oliver Benjamin
  * DataWriter class for writing user and question data to JSON files
- * 
-**/
-
+ */
 public class DataWriter extends DataConstants {
- 
+
     // ===================== USERS =====================
- 
+
     public static void saveUsers() {
         UserList userList = UserList.getInstance();
-        ArrayList<User> users = (ArrayList<User>) userList.getAllUsers();
+        ArrayList<User> users = userList.getAllUsers();
         JSONArray jsonUsers = new JSONArray();
- 
-        for (User user : users) {
-            jsonUsers.add(getUserJSON(user));
-        }
- 
+        for (User user : users) jsonUsers.add(getUserJSON(user));
+
         try (FileWriter file = new FileWriter(USER_FILE_NAME)) {
             file.write(jsonUsers.toJSONString());
             file.flush();
@@ -33,34 +27,31 @@ public class DataWriter extends DataConstants {
             e.printStackTrace();
         }
     }
- 
+
     public static JSONObject getUserJSON(User user) {
         JSONObject userDetails = new JSONObject();
         userDetails.put(USER_ID,       user.getUserId());
         userDetails.put(USER_USERNAME, user.getUsername());
         userDetails.put(USER_PASSWORD, user.getPassword());
         userDetails.put(USER_EMAIL,    user.getEmail());
-        userDetails.put(USER_TYPE,     user.getAccountType().toDisplayString());
- 
+        userDetails.put(USER_TYPE,     user.getAccountType().toString()); // FIXED: was USER_ACCOUNT_TYPE
+
         if (user instanceof Student) {
             Student s = (Student) user;
-            userDetails.put(USER_CLASSIFICATION, s.getClassification());
             userDetails.put(USER_STREAK,         s.getStreak());
+            userDetails.put(USER_CLASSIFICATION, s.getClassification());
         }
         return userDetails;
     }
- 
+
     // ===================== QUESTIONS =====================
- 
+
     public static void saveQuestions() {
         QuestionList questionList = QuestionList.getInstance();
-        ArrayList<Question> questions = (ArrayList<Question>) questionList.getAllQuestions();
+        ArrayList<Question> questions = questionList.getAllQuestions();
         JSONArray jsonQuestions = new JSONArray();
- 
-        for (Question question : questions) {
-            jsonQuestions.add(getQuestionJSON(question));
-        }
- 
+        for (Question question : questions) jsonQuestions.add(getQuestionJSON(question));
+
         try (FileWriter file = new FileWriter(QUESTION_FILE_NAME)) {
             file.write(jsonQuestions.toJSONString());
             file.flush();
@@ -68,89 +59,68 @@ public class DataWriter extends DataConstants {
             e.printStackTrace();
         }
     }
- 
+
     public static JSONObject getQuestionJSON(Question question) {
-        JSONObject questionDetails = new JSONObject();
- 
-        questionDetails.put(QUESTION_ID,                  question.getQuestionID().toString());
-        questionDetails.put(QUESTION_TITLE,               question.getTitle());
-        questionDetails.put(QUESTION_PROMPT,              question.getPrompt());
-        questionDetails.put(QUESTION_DIFFICULTY,          question.getDifficulty().toString());
-        questionDetails.put(QUESTION_LANGUAGE,            question.getLanguage().toString());
-        questionDetails.put(QUESTION_IS_SOLUTION_REVEALED, question.isSolutionRevealed());
-        questionDetails.put(QUESTION_SAMPLE_SOLUTION,     question.getSampleSolution());
-        questionDetails.put(QUESTION_SAMPLE_EXPLANATION,  question.getSampleExplanation());
- 
-        if (question.getCourse() != null) {
-            questionDetails.put(QUESTION_COURSE, question.getCourse().toString());
-        }
- 
-        if (question.getAuthor() != null) {
-            questionDetails.put(QUESTION_AUTHOR_ID, question.getAuthor().getUserId());
-        }
- 
-        // Courses array
-        JSONArray jsonCourses = new JSONArray();
-        for (Course course : question.getCourses()) {
-            jsonCourses.add(course.toString());
-        }
-        questionDetails.put(QUESTION_COURSES, jsonCourses);
- 
-        // Images
-        JSONArray jsonImages = new JSONArray();
-        for (String image : question.getImages()) {
-            jsonImages.add(image);
-        }
-        questionDetails.put(QUESTION_IMAGES, jsonImages);
- 
-        // Attachments
-        JSONArray jsonAttachments = new JSONArray();
-        for (String attachment : question.getAttachments()) {
-            jsonAttachments.add(attachment);
-        }
-        questionDetails.put(QUESTION_ATTACHMENTS, jsonAttachments);
- 
-        // Solutions
+        JSONObject q = new JSONObject();
+        q.put(QUESTION_ID,                  question.getQuestionID().toString());
+        q.put(QUESTION_TITLE,               question.getTitle());
+        q.put(QUESTION_PROMPT,              question.getPrompt());
+        q.put(QUESTION_DIFFICULTY,          question.getDifficulty().toString());
+        q.put(QUESTION_LANGUAGE,            question.getLanguage().toString());
+        q.put(QUESTION_COURSE,              question.getCourse().toString());
+        q.put(QUESTION_IS_SOLUTION_REVEALED, question.isSolutionRevealed());
+        q.put(QUESTION_SAMPLE_SOLUTION,     question.getSampleSolution());
+        q.put(QUESTION_SAMPLE_EXPLANATION,  question.getSampleExplanation());
+
+        if (question.getAuthor() != null)
+            q.put(QUESTION_AUTHOR_ID, question.getAuthor().getUserId());
+
+        // Solutions (answers)
         JSONArray jsonSolutions = new JSONArray();
-        for (Solution solution : question.getSolutions()) {
+        for (Solution solution : question.getSolutions())
             jsonSolutions.add(getSolutionJSON(solution));
-        }
-        questionDetails.put(QUESTION_SOLUTIONS, jsonSolutions);
- 
-        // Replies
+        q.put(QUESTION_SOLUTIONS, jsonSolutions);
+
+        // Replies (comments, recursive)
         JSONArray jsonReplies = new JSONArray();
-        for (Reply reply : question.getReplies()) {
+        for (Reply reply : question.getReplies())
             jsonReplies.add(getReplyJSON(reply));
-        }
-        questionDetails.put(QUESTION_REPLIES, jsonReplies);
- 
-        return questionDetails;
+        q.put(QUESTION_REPLIES, jsonReplies);
+
+        return q;
     }
- 
+
     // ===================== SOLUTIONS =====================
- 
+
     public static JSONObject getSolutionJSON(Solution solution) {
-        JSONObject solutionDetails = new JSONObject();
-        solutionDetails.put(SOLUTION_ID,      solution.getSolutionId().toString());
-        solutionDetails.put(SOLUTION_CONTENT, solution.getContent());
-        if (solution.getAuthor() != null) {
-            solutionDetails.put(SOLUTION_AUTHOR_ID, solution.getAuthor().getUserId());
-        }
-        return solutionDetails;
+        JSONObject s = new JSONObject();
+        s.put(SOLUTION_ID,       solution.getSolutionId().toString());
+        s.put(SOLUTION_CONTENT,  solution.getContent());
+        s.put(SOLUTION_UPVOTES,  solution.getUpvotes());
+        s.put(SOLUTION_ACCEPTED, solution.isAccepted());
+        if (solution.getAuthor() != null)
+            s.put(SOLUTION_AUTHOR_ID, solution.getAuthor().getUserId());
+        return s;
     }
- 
-    // ===================== REPLIES =====================
- 
+
+    // ===================== REPLIES (recursive) =====================
+
     public static JSONObject getReplyJSON(Reply reply) {
-        JSONObject replyDetails = new JSONObject();
-        replyDetails.put(REPLY_ID,      reply.getReplyId().toString());
-        replyDetails.put(REPLY_CONTENT, reply.getContent());
-        if (reply.getTitle() != null) {
-            replyDetails.put(REPLY_TITLE, reply.getTitle());
-        }
-        if (reply.getAuthor() != null) {
-            replyDetails.put(REPLY_AUTHOR_ID, reply.getAuthor().getUserId());
-        }
-        return replyDetails;
+        JSONObject r = new JSONObject();
+        r.put(REPLY_ID,       reply.getReplyId().toString());
+        r.put(REPLY_TITLE,    reply.getTitle());
+        r.put(REPLY_CONTENT,  reply.getContent());
+        r.put(REPLY_UPVOTES,  reply.getUpvotes());
+        r.put(REPLY_ACCEPTED, reply.isAccepted());
+        if (reply.getAuthor() != null)
+            r.put(REPLY_AUTHOR_ID, reply.getAuthor().getUserId());
+
+        // Recursive: save nested replies (comments on comments)
+        JSONArray nestedReplies = new JSONArray();
+        for (Reply nested : reply.getReplies())
+            nestedReplies.add(getReplyJSON(nested));
+        r.put(QUESTION_REPLIES, nestedReplies);
+
+        return r;
     }
 }
